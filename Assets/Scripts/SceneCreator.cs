4ec555a69347c18bc25885f888159817cd68cd5a
CCapslock <53517856+CCapslock@users.noreply.github.com>
2020-10-18 14:47:16 +0300

    воркинг

--- Assets/Scripts/SceneCreator.cs
+++ Assets/Scripts/SceneCreator.cs
@@ -3,6 +3,7 @@
 public class SceneCreator : MonoBehaviour
 {
 	public GameObject PlaceAbleObjectPrefab;
+	public GameObject PlaceAbleObjectPrefabForTesting;
 	public GameObject SingleBlockPrefab;
 	private Camera _camera;
 	private Vector2[,] _spawnPoints;
@@ -22,20 +23,48 @@ public class SceneCreator : MonoBehaviour
 		_hight = _maxScreenPosition.y * 2;
 		_row = Row;
 		_column = Column;
-		float xnum = _widht / (_column + 1);
-		float ynum = _hight / (_row + 1);
-		for (int i = 1; i <= Row; i++)
+		//float xnum = _widht / (_column + 1);
+		//float ynum = _hight / (_row + 1);
+		SpriteRenderer PlaceAbleBlockSprite = PlaceAbleObjectPrefabForTesting.GetComponent<SpriteRenderer>();
+		Debug.Log("PlaceAbleBlock Size: " + PlaceAbleBlockSprite.sprite.bounds.size.y * PlaceAbleObjectPrefab.transform.localScale.y + " Screen Hight: " + _maxScreenPosition.y * 2);
+		/*for (int i = 1; i <= Row; i++)
 		{
 			for (int j = 1; j <= Column; j++)
 			{
 				Vector2 pposition = new Vector2(_minScreenPosition.x + xnum * j, _minScreenPosition.y + ynum * i);
 				Instantiate(PlaceAbleObjectPrefab, pposition, Quaternion.identity);
 			}
+		}*/
+
+
+
+
+		float BlockSize = PlaceAbleBlockSprite.sprite.bounds.size.x * PlaceAbleObjectPrefab.transform.localScale.x;
+		float StartXPosition = 0 - BlockSize * (_column / 2);
+		float StartYPosition = 0 - BlockSize * (_row / 2);
+		for (int i = 0; i < _row; i++)
+		{
+			for (int j = 0; j < _column; j++)
+			{
+				Vector2 pposition = new Vector2(StartXPosition + BlockSize * j, StartYPosition + BlockSize * i);
+				Instantiate(PlaceAbleObjectPrefab, pposition, Quaternion.identity);
+			}
 		}
 	}
+
+	private void ChangePlaceAbleBlock()
+	{
+
+	}
+
 	//создает точки спавна для SpawnerController
 	public Vector2[,] CreateSpawnPoints(int Row, int Column)
 	{
+		SpriteRenderer PlaceAbleBlockSprite = PlaceAbleObjectPrefabForTesting.GetComponent<SpriteRenderer>();
+		float BlockSize = PlaceAbleBlockSprite.sprite.bounds.size.x * PlaceAbleObjectPrefab.transform.localScale.x;
+		float StartXPosition = 0 - BlockSize * (_column / 2);
+		float StartYPosition = _maxScreenPosition.y + BlockSize;
+
 		_spawnPoints = new Vector2[3, Column];
 		float xnum = _widht / (Column + 1);
 		float ynum = _hight / (Row + 1);
@@ -43,8 +72,8 @@ public class SceneCreator : MonoBehaviour
 		{
 			for (int j = 0; j < Column; j++)
 			{
-				_spawnPoints[i, j].x = _minScreenPosition.x + xnum * (j + 1);
-				_spawnPoints[i, j].y = _maxScreenPosition.y + ynum * (i + 1);
+				_spawnPoints[i, j].x = StartXPosition + BlockSize * (j);
+				_spawnPoints[i, j].y = StartYPosition + BlockSize * (i);
 			}
 		}
 		return _spawnPoints;
