9e3556783ae4d6f3693b69d6325e6868813bf7d5
CCapslock <53517856+CCapslock@users.noreply.github.com>
2020-10-29 10:09:57 +0300

    убрал нью из апдейта

--- Assets/Scripts/scroll.cs
+++ Assets/Scripts/scroll.cs
@@ -4,11 +4,17 @@ public class scroll : MonoBehaviour
 {
     public float scrollspeed;
     public Material material;
+    private Vector2 _scrollingVector;
     private int MainTex = Shader.PropertyToID("_MainTex");
 
+    private void Awake()
+    {
+        _scrollingVector = new Vector2();
+    }
     private void Update()
     {
         var offset = Time.time * scrollspeed;
-        material.SetTextureOffset(MainTex, new Vector2(0, -offset));
+        _scrollingVector.y = -offset;
+        material.SetTextureOffset(MainTex, _scrollingVector);
     }
 }
