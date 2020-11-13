a2b7a115b7e1372d117efd401ff792eb38864970
CCapslock <53517856+CCapslock@users.noreply.github.com>
2020-11-13 19:56:43 +0300

    настроил звук, теперь сохраняется

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
@@ -6,11 +7,29 @@ public class SoundController : MonoBehaviour
 	[SerializeField] private AudioSource _audioSourceLine;
 	[SerializeField] private AudioSource _audioSourceMusic;
 	[SerializeField] private AudioSource _audioSourceButtonClick;
+	public Image MusicButton;
+	public Image SoundButton;
+	public Sprite MusicOnSprite;
+	public Sprite SoundOnSprite;
+	public Sprite MusicOffSprite;
+	public Sprite SoundOffSprite;
 	public bool SoundOn;
 	public bool MusicOn;
+	// player prefs : SoundOff если 1 то true
+	//				  MusicOff если 1 то true
 
 	private void Start()
 	{
+		if (PlayerPrefs.GetInt("SoundOnSprite") == 1)
+		{
+			SoundOn = false;
+			SoundButton.sprite = SoundOffSprite;
+		}
+		if (PlayerPrefs.GetInt("MusicOff") == 1)
+		{
+			MusicOn = false;
+			MusicButton.sprite = MusicOffSprite;
+		}
 		PlayMusic();
 	}
 	public void ChangeSoundBool()
@@ -18,10 +37,14 @@ public class SoundController : MonoBehaviour
 		if (SoundOn)
 		{
 			SoundOn = false;
+			PlayerPrefs.SetInt("SoundOnSprite", 1);
+			SoundButton.sprite = SoundOffSprite;
 		}
 		else
 		{
 			SoundOn = true;
+			PlayerPrefs.SetInt("SoundOnSprite", 0);
+			SoundButton.sprite = SoundOnSprite;
 			PlayButtonClick();
 		}
 	}
@@ -30,12 +53,16 @@ public class SoundController : MonoBehaviour
 		if (MusicOn)
 		{
 			MusicOn = false;
+			PlayerPrefs.SetInt("MusicOff", 1);
+			MusicButton.sprite = MusicOffSprite;
 			_audioSourceMusic.Stop();
 			PlayButtonClick();
 		}
 		else
 		{
 			MusicOn = true;
+			PlayerPrefs.SetInt("MusicOff", 0);
+			MusicButton.sprite = MusicOnSprite;
 			PlayButtonClick();
 			PlayMusic();
 		}
